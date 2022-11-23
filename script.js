e78ce9b88e922ee4ed3e765e7b16f050cea99916
RomainNgd <romain.nigond@gmail.com>
2022-11-23 15:06:53 +0100

    page d'accueilv1

---
+++ script.js
@@ -0,0 +1,15 @@
+$(function() {
+    $('.owl-carousel.testimonial-carousel').owlCarousel({
+        nav: true,
+        navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+        dots: false,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            750: {
+                items: 2,
+            }
+        }
+    });
+});
