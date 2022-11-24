9b5020f7473417bfaf4d5b969158f18056f2ac6f
RomainNgd <romain.nigond@gmail.com>
2022-11-24 15:20:37 +0100

    last fix

--- data/js/script.js
+++ data/js/script.js
@@ -4,19 +4,31 @@ let container = document.getElementById("carrousel-container");
 let droite = document.getElementById("d");
 let gauche = document.getElementById("g");
 
-container.style.width = (800*nbr) + "px";
+container.style.width = (600*nbr) + "px";
 
 gauche.onclick=function (){
     if (p>-nbr+1)
         p--;
-    container.style.transform="translate(" +p*800+"px)"
-    container.style.transition="all 0.5s ease"
+    if (window.innerWidth < 700 ){
+        container.style.transform="translate(" +p*300+"px)"
+        container.style.transition="all 0.5s ease"
+    }
+    else {
+        container.style.transform="translate(" +p*600+"px)"
+        container.style.transition="all 0.5s ease"
+    }
 }
 
 
 droite.onclick=function (){
     if (p<0)
         p++;
-    container.style.transform="translate(" +p*800+"px)"
-    container.style.transition="all 0.5s ease"
+    if (window.innerWidth < 700 ){
+        container.style.transform="translate(" +p*300+"px)"
+        container.style.transition="all 0.5s ease"
+    }
+    else {
+        container.style.transform="translate(" +p*600+"px)"
+        container.style.transition="all 0.5s ease"
+    }
 }
